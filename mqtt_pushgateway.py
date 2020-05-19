51159f4d867b047607a0601892d395d2d0503202
Marcin Badurowicz <m@badurowicz.net>
2020-05-19 12:00:10 +0200

    Change into a MQTT-Pushgateway proxy using HTTP requests
    Whole Flask app has been removed, everything else stays more or less the same

--- mqtt_pushgateway.py
+++ mqtt_pushgateway.py
@@ -8,30 +8,28 @@ import logging
 import socket
 import time
 import json
+import sys
+import base64
+import requests
 
 import paho.mqtt.client as mqtt
 
 from collections import defaultdict
-from datetime    import datetime, timedelta
-
-from flask import Flask, Response, redirect
-
-app = Flask("mqtt_pushgateway")
+from datetime import datetime, timedelta
 
 with open("config.toml") as fd:
     config = pytoml.load(fd)
 
-
 class Topic(object):
     def __init__(self):
-        self.metric      = None
-        self.keywords    = {}
-        self.value       = None
+        self.metric = None
+        self.keywords = {}
+        self.value = None
         self.last_update = datetime.fromtimestamp(0)
-        self.expire      = config["mqtt"].get("expire")
-        self.ignore      = False
-        self.known_vals  = set([])
-        self.is_numeric  = True
+        self.expire = config["mqtt"].get("expire")
+        self.ignore = False
+        self.known_vals = set([])
+        self.is_numeric = True
 
     def update(self, topic, value):
         # topic is e.g. sensors/somewhere/temperature
@@ -69,6 +67,11 @@ class Topic(object):
 
         self.last_update = datetime.now()
 
+        print(str(self))
+        response = requests.post(f'{config["pushgw"]["address"]}/metrics/job/{self.metric}', data=str(
+            self), auth=(config["pushgw"]["username"], config["pushgw"]["password"]))
+        response.raise_for_status()
+
     @property
     def forget(self):
         return datetime.now() - self.last_update > timedelta(hours=1)
@@ -79,32 +82,35 @@ class Topic(object):
         if self.is_numeric:
             if self.expire is not None and data_age > self.expire:
                 # metric is expired, return data age only
-                template =  'mqtt_data_age{%(kwds)s,metric="%(metric)s"} %(age)f'
+                template = 'mqtt_data_age{%(kwds)s,metric="%(metric)s"} %(age)f'
             else:
                 template = ('%(metric)s{%(kwds)s} %(value)f\n'
-                            'mqtt_data_age{%(kwds)s,metric="%(metric)s"} %(age)f')
+                            'mqtt_data_age{%(kwds)s,metric="%(metric)s"} %(age)f\n')
 
             return template % dict(
-                metric = self.metric,
-                kwds   = ','.join([ '%s="%s"' % item for item in self.keywords.items() ]),
-                value  = self.value,
-                age    = data_age
+                metric=self.metric,
+                kwds=','.join(
+                    ['%s="%s"' % item for item in self.keywords.items()]),
+                value=self.value,
+                age=data_age
             )
 
         else:
             series = ['mqtt_data_age{%(kwds)s,metric="%(metric)s"} %(age)f' % dict(
-                metric = self.metric,
-                kwds   = ','.join([ '%s="%s"' % item for item in self.keywords.items() ]),
-                age    = data_age
+                metric=self.metric,
+                kwds=','.join(
+                    ['%s="%s"' % item for item in self.keywords.items()]),
+                age=data_age
             )]
             if self.expire is None or data_age < self.expire:
                 for known_val in self.known_vals:
                     # generate one time series for each known value, where the value is 1
                     # for the current value and 0 for all else
                     series.append('%(metric)s{%(kwds)s} %(value)f' % dict(
-                        metric = self.metric,
-                        kwds   = ','.join([ '%s="%s"' % item for item in dict(self.keywords, **{self.metric: known_val}).items() ]),
-                        value  = int(known_val == self.value)
+                        metric=self.metric,
+                        kwds=','.join(['%s="%s"' % item for item in dict(
+                            self.keywords, **{self.metric: known_val}).items()]),
+                        value=int(known_val == self.value)
                     ))
             return "\n".join(series)
 
@@ -112,28 +118,17 @@ class Topic(object):
 metrics = defaultdict(Topic)
 
 
-@app.route("/")
-def http_index():
-    return redirect("/metrics", code=302)
-
-@app.route("/metrics")
-def http_metrics():
-    content = [str(metric)
-        for metric in metrics.values()
-        if not metric.ignore and not metric.forget
-    ]
-    return Response('\n'.join(content + ['']), mimetype="text/plain")
-
-
 def on_message(client, userdata, message):
     topic = message.topic
+
     try:
         payload = message.payload.decode("utf-8")
     except:
-        logging.warning("Payload for '%s' is not valid utf-8, ignored" % topic, exc_info=True)
+        print("Payload for '%s' is not valid utf-8, ignored" %
+              topic, exc_info=True)
     else:
         payload = payload.strip()
-        logging.info("Message received: %s => %s", topic, payload)
+        print(f"Message received: {topic} => {payload}")
 
     if payload[0] == "{" and payload[-1] == "}":
         try:
@@ -150,37 +145,32 @@ def on_message(client, userdata, message):
     try:
         metrics[topic].update(topic, payload)
     except:
-        logging.warning("Metric update for '%s' failed" % topic, exc_info=True)
+        print("Metric update for '%s' failed" % topic, exc_info=True)
+
 
 def main():
     client = mqtt.Client(config["mqtt"]["client_id"] % dict(
         hostname=socket.gethostname()
     ))
-    client.username_pw_set(config["mqtt"]["username"], config["mqtt"]["password"])
+    #client.username_pw_set(config["mqtt"]["username"], config["mqtt"]["password"])
     client.on_message = on_message
 
     def on_connect(client, userdata, flags, result):
-        logging.info("subscribing")
+        print("subscribing")
         for topic in config["mqtt"]["subscribe"]:
+            print(topic)
             client.subscribe(topic)
 
     client.on_connect = on_connect
-
-
     client.connect(config["mqtt"]["broker"], port=config["mqtt"]["port"])
 
-    client.loop_start()
+    client.loop_forever()
 
+    # client.disconnect()
+    # client.loop_stop()
 
-    app.debug = False
-
-    try:
-        app.run(host=config["exporter"]["listen"], port=config["exporter"]["port"])
-    except KeyboardInterrupt:
-        print("exiting")
-
-    client.disconnect()
-    client.loop_stop()
+    # while True:
+    # pass
 
 
 if __name__ == '__main__':
